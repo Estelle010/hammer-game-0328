c52f2ed206ef71bd75ef79bb78c61700c8ce32c5
Estelle010 <102577678+Estelle010@users.noreply.github.com>
2022-03-29 15:33:23 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1648539024,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/hole-sheet0.png",
+		"images/ham-sheet0.png",
+		"images/bad-sheet0.png",
+		"media/mixkit-gearbox-working-2046.ogg",
+		"media/mixkit-gearbox-working-2046.m4a",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
